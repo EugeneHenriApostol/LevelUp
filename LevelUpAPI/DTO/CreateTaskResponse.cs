e960e3484ea2d1e7146f0ff84ea46d9c603c0fb0
TRAINEE - BIHAG <zabihag@n-pax.com>
2025-09-08 13:30:36 +0800

    registrationPUT

--- LevelUpAPI/DTO/CreateTaskResponse.cs
+++ LevelUpAPI/DTO/CreateTaskResponse.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LevelUpAPI.Models
+namespace LevelUpAPI.DTO
 {
-    public class Task
+    public class CreateTaskResponse
     {
         public int TaskId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime DueDate { get; set; }
 
-        // foreign key to trainer
         public int CreatedById { get; set; }
-        public User CreatedBy { get; set; } = null!;
+        public string CreatedByName { get; set; } = string.Empty;
+
     }
 }
