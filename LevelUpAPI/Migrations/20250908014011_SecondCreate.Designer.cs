4b819ec0348b45eeb3d8aabb1c9fbd593d93954e
malscyber <maloutlot@gmail.com>
2025-09-08 09:44:16 +0800

    Added http delete {id}

--- LevelUpAPI/Migrations/20250908014011_SecondCreate.Designer.cs
+++ LevelUpAPI/Migrations/20250908014011_SecondCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LevelUpAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LevelUpAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250908014011_SecondCreate")]
+    partial class SecondCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
