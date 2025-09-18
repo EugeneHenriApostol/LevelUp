e3e8d3e88b36b706e90197262e0784b28f6e2961
TRAINEE - APOSTOL <egapostol@n-pax.com>
2025-09-18 16:28:08 +0800

    added submission apis, get apis, modified create task api, added basecontroller as helper function in getting the currentuserId

--- LevelUpAPI/DTO/CreateTaskDto.cs
+++ LevelUpAPI/DTO/CreateTaskDto.cs
@@ -2,18 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace LevelUpAPI.DTO
 {
     public class CreateTaskDto
     {
+        [Required]
         public string TaskTitle { get; set; } = string.Empty;
+        [Required]
         public string TaskDescription { get; set; } = string.Empty;
+        [Required]
         public DateTime DueDate { get; set; }
-
-        // foreign key: trainer creating the task
-        public int CreatedById { get; set; }
-        public List<int>? AssignedTraineeIds { get; set; }
+        // Optional: List of trainees to assign
+        // If null or empty, assign to all trainees
+        // public List<int>? AssignedTraineeIds { get; set; }
+        [Range(1, 1000)]
         public int Points { get; set; } = 0;
     }
 }
