6e70cfac673640d2f77e625e223dd0822686a340
TRAINEE - APOSTOL <egapostol@n-pax.com>
2025-09-19 15:35:49 +0800

    integrated login api and fixed cors issues

--- LevelUpFrontend/src/App.jsx
+++ LevelUpFrontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage"; 
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
+import OverviewTraineePage from "./components/overviewTrainee";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/landingpage" element={<LandingPage />} />
         <Route path="/loginpage" element={<LoginPage />} />
         <Route path="/signuppage" element={<SignupPage />} />
+        <Route path="/overviewtrainee" element={<OverviewTraineePage />} />
       </Routes>
     </Router>
   );
